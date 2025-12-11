1ee2a3305df8cf7c118074c2c2cde2142c8dd9d4
Paul Wisner <pskwisner@Pauls-MacBook-Air.local>
2025-12-10 23:21:11 -0500

    grid create function

--- src/grid.ml
+++ src/grid.ml
@@ -9,3 +9,15 @@ type 'a t = {
 }
 
 let size g = g.size
+
+let create ~size ~init =
+  let positions =
+    List.init size ~f:(fun y ->
+        List.init size ~f:(fun x -> Position.{ x; y }))
+    |> List.concat
+  in
+  let data =
+    List.fold positions ~init:PosMap.empty ~f:(fun m pos ->
+        Map.set m ~key:pos ~data:(init pos))
+  in
+  { size; data }
--- src/grid.mli
+++ src/grid.mli
@@ -3,3 +3,5 @@ module Position = Position
 type 'a t
 
 val size : 'a t -> int
+
+val create : size:int -> init:(Position.t -> 'a) -> 'a t
