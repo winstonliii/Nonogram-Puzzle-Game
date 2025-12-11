e3864822d98f0ef9dab2973f71c8be62b201d69b
Paul Wisner <pskwisner@Pauls-MacBook-Air.local>
2025-12-11 12:19:43 -0500

    puzzle uses grid

--- src/puzzle.ml
+++ src/puzzle.ml
@@ -8,63 +8,37 @@ type cell_state =
 type clue = RLE of int list [@@unboxed]
 
 module Position = Position
-module PosMap = Map.Make (Position)
 
 type position = Position.t
 
 type t = {
   size : int;
-  grid : cell_state PosMap.t;
+  grid : cell_state Grid.t;
   row_clues : clue array;
   col_clues : clue array;
 }
 
 let size p = p.size
 
-let get p pos =
-  match Map.find p.grid pos with
-  | Some state -> state
-  | None -> Unknown
+let get p pos = Grid.get p.grid pos
 
 let set p pos state =
-  let grid =
-    match state with
-    | Unknown -> Map.remove p.grid pos
-    | _ -> Map.set p.grid ~key:pos ~data:state
-  in
+  let grid = Grid.set p.grid pos state in
   { p with grid }
 
-let rows p r =
-  List.init p.size ~f:(fun x ->
-      get p Position.{ x; y = r })
+let rows p r = Grid.rows p.grid r
 
-let cols p c =
-  List.init p.size ~f:(fun y ->
-      get p Position.{ x = c; y })
+let cols p c = Grid.cols p.grid c
 
-let fold_rows p ~init ~f =
-  let rec loop r acc =
-    if r = p.size then acc
-    else
-      let row_cells = rows p r in
-      let acc' = f acc r row_cells in
-      loop (r + 1) acc'
-  in
-  loop 0 init
+let fold_rows p ~init ~f = Grid.fold_rows p.grid ~init ~f
 
-let fold_cols p ~init ~f =
-  let rec loop c acc =
-    if c = p.size then acc
-    else
-      let col_cells = cols p c in
-      let acc' = f acc c col_cells in
-      loop (c + 1) acc'
-  in
-  loop 0 init
+let fold_cols p ~init ~f = Grid.fold_cols p.grid ~init ~f
 
 let row_clue p r = p.row_clues.(r)
 let col_clue p c = p.col_clues.(c)
 
 let create ~size ~row_clues ~col_clues =
-  let grid = PosMap.empty in
+  let grid =
+    Grid.create ~size ~init:(fun _ -> Unknown)
+  in
   { size; grid; row_clues; col_clues }
