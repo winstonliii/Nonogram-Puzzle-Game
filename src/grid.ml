dc17722835cdd88ac0964b2c95bd1d50c42b7c0e
Paul Wisner <pskwisner@Pauls-MacBook-Air.local>
2025-12-11 00:05:28 -0500

    moving functions from puzzle to grid

--- src/grid.ml
+++ src/grid.ml
@@ -21,3 +21,39 @@ let create ~size ~init =
         Map.set m ~key:pos ~data:(init pos))
   in
   { size; data }
+
+let get g pos =
+  match Map.find g.data pos with
+  | Some v -> v
+  | None -> invalid_arg "Position not in grid"
+
+let set g pos v =
+  { g with data = Map.set g.data ~key:pos ~data:v }
+
+let rows g r =
+  List.init g.size ~f:(fun x ->
+    get g Position.{ x; y = r })
+
+let cols g c =
+  List.init g.size ~f:(fun y ->
+      get g Position.{ x = c; y })
+
+let fold_rows g ~init ~f =
+  let rec loop r acc =
+    if r = g.size then acc
+    else
+      let row_cells = rows g r in
+      let acc' = f acc r row_cells in
+      loop (r + 1) acc'
+  in
+  loop 0 init
+
+let fold_cols g ~init ~f =
+  let rec loop c acc =
+    if c = g.size then acc
+    else
+      let col_cells = cols g c in
+      let acc' = f acc c col_cells in
+      loop (c + 1) acc'
+  in
+  loop 0 init
--- src/grid.mli
+++ src/grid.mli
@@ -5,3 +5,23 @@ type 'a t
 val size : 'a t -> int
 
 val create : size:int -> init:(Position.t -> 'a) -> 'a t
+
+val get : 'a t -> Position.t -> 'a
+
+val set : 'a t -> Position.t -> 'a -> 'a t
+
+val rows : 'a t -> int -> 'a list
+
+val cols : 'a t -> int -> 'a list
+
+val fold_rows :
+  'a t ->
+  init:'b ->
+  f:('b -> int -> 'a list -> 'b) ->
+  'b
+
+val fold_cols :
+  'a t ->
+  init:'b ->
+  f:('b -> int -> 'a list -> 'b) ->
+  'b
